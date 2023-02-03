c04c004bd6cadecc102c85b2c0c89a04a4bb4433
Patrick Roza <contact@patrickroza.com>
2023-02-03 07:15:33 +0100

    Play with "tsplus/ForceLazyArgument"

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -33780,6 +33780,15 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         }
         return false;
     }
+    function isForceLazyParameterByType(type: Type): type is TypeReference {
+        if (type.symbol && type.symbol.declarations && type.symbol.declarations.length > 0) {
+            const tag = collectTsPlusTypeTags(type.symbol.declarations[0])[0];
+            if (tag === "tsplus/ForceLazyArgument") {
+                return true;
+            }
+        }
+        return false;
+    }
     // TSPLUS EXTENSION END
 
     function getMutableArrayOrTupleType(type: Type) {
@@ -34016,7 +34025,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
             // TSPLUS EXTENTION START
             const originalParamType = thisType;
             let paramType = originalParamType;
-            if (isLazyParameterByType(originalParamType) && thisArgumentNode) {
+            if ((isLazyParameterByType(originalParamType) || isForceLazyParameterByType(originalParamType)) && thisArgumentNode) {
                 const contextFreeArgType = thisArgumentType;
                 if (isTypeIdenticalTo(contextFreeArgType, anyType) || isTypeIdenticalTo(contextFreeArgType, neverType)) {
                     return [createDiagnosticForNode(
@@ -34050,7 +34059,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 const originalParamType = getTypeAtPosition(signature, i);
                 const argType = checkExpressionWithContextualType(arg, unionIfLazy(originalParamType), /*inferenceContext*/ undefined, checkMode);
                 let paramType = originalParamType;
-                if (isLazyParameterByType(originalParamType)) {
+                if (isLazyParameterByType(originalParamType) || isForceLazyParameterByType(originalParamType)) {
                     if ((isTypeIdenticalTo(argType, anyType) || isTypeIdenticalTo(argType, neverType)) && !(checkMode & CheckMode.SkipGenericFunctions)) {
                         return [createDiagnosticForNode(
                             arg,
